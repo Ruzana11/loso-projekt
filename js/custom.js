4240c90e335401a782624076213d847f2a1eabcf
markusphil <m.philipp@one-dot.de>
2021-02-10 17:27:30 +0100

    Animation Lib

--- js/custom.js
+++ js/custom.js
@@ -154,6 +154,22 @@ $(document).ready(function(){
     });
 });
 
+//Counter JS LIB
+
+$(document).ready(function () {
+    $('.counter-num').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animation JS
+$(document).ready(function () {
+    'use strict';
+    new WOW().init();
+});
+
+
 
 
 
